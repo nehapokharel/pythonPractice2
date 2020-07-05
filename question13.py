97a68e9785747d9d1fd058e25fc38e7d01fd43c2
Neha Pokharel <nehapokharel008@gmail.com>
2020-07-05 20:04:46 +0545

    solution of question 13

---
+++ question13.py
@@ -0,0 +1,10 @@
+import csv
+def create_csv(file_name, values):
+    with open(file_name, 'w') as per:
+        writer = csv.writer(per)
+        writer.writerow(['name', 'adderss', 'age'])
+        writer.writerows(values)
+        
+def main():
+    info = [('George', '4312 Abbey Road', 22), ('John', '54 Love Ave', 21)]
+    create_csv('info.csv', info)
